1fb427f1fc2c3e493071800bcfd5ff69d866b440
AllanVic0 <147013844+AllanVic0@users.noreply.github.com>
2025-02-05 15:31:18 -0300

    Update ex045.py

--- ex045.py
+++ ex045.py
@@ -1,6 +1,38 @@
 from random import randint
 
+print('''JOKENPÔ
+      [0] Pedra
+      [1] Papel
+      [2] Tesoura''')
+
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
-print('O PC jogou {}'.format(itens[computador]))
-jogador = randint(0, 2)
+
+jogador = int(input('Faça sua jogada: '))
+
+print('O PC jogou {}: '.format(itens[computador]))
+print('O jogador jogou {}: '.format(itens[jogador]))
+
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE!')
+    elif jogador == 1:
+        print('Jogador venceu')
+    elif jogador == 2:
+        print('Computador venceu')
+elif computador == 1:
+    if jogador == 0:
+        print('Computador venceu!')
+    elif jogador == 1:
+        print('EMPATE!')
+    elif jogador == 2:
+        print('Jogador venceu')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador venceu!')
+    elif jogador == 1:
+        print('Computador venceu!')
+    elif jogador == 2:
+        print('EMPATE!')
+else:
+    print('Jogada inválida')
